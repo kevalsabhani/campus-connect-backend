5a711d38e5e1664f7d561acf7db21f90d8c70e7b
kevalsabhani <kevalsabhani@gmail.com>
2024-10-04 00:17:37 +0530

    Add graceful shutdown

--- cmd/campus_connect/main.go
+++ cmd/campus_connect/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
 	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/kevalsabhani/campus-connect-backend/internal/config"
 )
 
@@ -11,11 +18,38 @@ func main() {
 
 	// Load config
 	cfg := config.MustLoad()
-	slog.Info("Loaded config", "config", cfg)
 
 	// Database setup
 
 	// setup router
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	}).Methods("GET")
 
-	// Run server
+	server := &http.Server{
+		Addr:    cfg.Server.Host + ":" + cfg.Server.Port,
+		Handler: r,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		slog.Info("starting server", "host", cfg.Server.Host, "port", cfg.Server.Port)
+		if err := server.ListenAndServe(); err != nil {
+			slog.Error("failed to start server", "error", err)
+		}
+	}()
+
+	<-done
+	slog.Info("shutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", "error", err)
+	}
+
+	slog.Info("server shutdown successfully")
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.1
 
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
